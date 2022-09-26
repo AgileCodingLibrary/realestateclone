80d6ce77b67d634807370320af09d0b31b1ea605
AgileCodingLibrary <46870899+AgileCodingLibrary@users.noreply.github.com>
2022-09-26 10:28:01 +0100

    Install react router and create pages.

--- realestatecloneapp/src/App.js
+++ realestatecloneapp/src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 function App() {
-  return <div className='text bg-lime-300'>Real estate clone</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
